8a36fc62c4f11257b7c185dada603e19c3a3307e
abhinav <nabhinav3@gatech.edu>
2014-01-13 20:41:13 -0500

    adding the new link list c file which does not keep a global ll head pointer and corresponding merger with meddle source code

--- src/link_list.c
+++ src/link_list.c
@@ -2,33 +2,36 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include "link_list.h"
+struct node{
+  u_char* data;
+  int data_len;
+  struct node *next;
+};
 
-
-
-
+typedef struct node node;
 static list_size=0;
-node * head =NULL;
-int beg_add_element(u_char *blob,int blob_size)
+
+int beg_add_element(node ** p_head ,u_char *blob,int blob_size)
 {
   struct node * element= (struct node *) malloc (sizeof(struct node));
   memset(element, 0, sizeof(element));
   element->data = malloc(blob_size);
-
-  if (element->data ==NULL)
+  
+  if (element->data ==NULL){
+    printf("malloc failed in beg_add_element()\n");
     return -1;
-
+  }
   element->data_len=blob_size;
   memcpy(element->data,blob,blob_size);
-  if (head ==NULL)
+  if (*p_head ==NULL)
     {
-      head =element;
-      head->next = NULL;
+      *p_head =element;
+      (*p_head)->next = NULL;
     }
   else
     {
-      element->next =head;
-      head = element;
+      element->next =*p_head;
+      *p_head = element;
     }
   list_size++;
 }
@@ -36,21 +39,24 @@ int beg_add_element(u_char *blob,int blob_size)
 Adds the packet buffer and the packet buffer length to the linked 
 list.
 */
-int end_add_element(u_char * blob, int blob_size)
-{
+int end_add_element(node **p_head , u_char * blob, int blob_size)
+{  
   node * temp;
+
   node * element= (node *) malloc (sizeof(struct node));
   memset(element, 0, sizeof(element));
   element->data = malloc(blob_size);
-  if (element->data ==NULL)
+  if (element->data ==NULL){
+    printf("malloc failed\n");
     return -1;
+  }
   element->data_len = blob_size;
   memcpy(element->data,blob,blob_size);
-  temp = head ;
-  if (head ==NULL)
+  temp = *p_head ;
+  if (*p_head ==NULL)
     {
-      head = element;
-      head->next = NULL;
+      *p_head = element;
+      (*p_head)->next = NULL;
     }
   else
     {
@@ -66,10 +72,11 @@ int end_add_element(u_char * blob, int blob_size)
 /*
 Prints the contents of the linked list starting from head pointer
 */
-int print_list()
+int print_list(node *p)
 {
+  printf("in print list\n");
   node * start ;
-  start= head ; 
+  start= p ;
   int idx =0;
   while (start !=NULL)
     {
@@ -81,15 +88,16 @@ int print_list()
 /*
   Fetches the packet buffer and the packet len from the linked list 
 */
-int delete_element( u_char** fetch_data, int *fetch_data_len )
+int beg_del_element( node **p_head, u_char** fetch_data, int *fetch_data_len )
 {
   node * fetch_node;
-  fetch_node = head ;
-  if (fetch_node->next == NULL && list_size==0)
+  fetch_node = *p_head ;
+  if (fetch_node ==NULL && list_size==0)
     {
+      printf("list is empty\n");
       return -1; //empty list
     }
-  head=head->next;
+  *p_head=fetch_node->next;
   *fetch_data = malloc(fetch_node->data_len);
   memset(*fetch_data,0, fetch_node->data_len);
   memcpy(*fetch_data,fetch_node->data,fetch_node->data_len);
@@ -97,26 +105,35 @@ int delete_element( u_char** fetch_data, int *fetch_data_len )
   list_size--;
 }
 
-static int test_suit()
+int test_suit()
 {
-  end_add_element((u_char*)"abhinav", sizeof("abhinav"));
-  end_add_element((u_char*)" narain", sizeof(" narain"));
-  end_add_element((u_char*)"this is a test suit",sizeof("this is a test suit"));
-  end_add_element((u_char*)"this is another line in the test suite",sizeof("this is another line in the test suite"));
-  end_add_element((u_char*)"the last line that is ever going to be written in the test suite",\
-		  sizeof("the last line that is ever going to be written in the test suite"));
+  node * head =NULL;
+  beg_add_element(&head, "abhinav", sizeof("abhinav"));
+  beg_add_element(&head," narain", sizeof(" narain"));
+  beg_add_element(&head,"this is a test suit",sizeof("this is a test suit"));
+  //  end_add_element(&head,"this is another line in the test suite",sizeof("this is another line in the test suite"));
+  //end_add_element(&head, "the last line that is ever going to be written in the test suite", \
+  //		  sizeof("the last line that is ever going to be written in the test suite"));
   printf("done with adding elements\n");
-  print_list();
+  if (head ==NULL)
+    printf("head is null ");
+  print_list(head);
   u_char * d1;
   u_char * d2;
   int l1,l2;
-  printf("%p",d1);
-  delete_element(&d1,&l1);
+  beg_del_element(&head, &d1, &l1);
   printf("the stuff that we got: %s %d\n",d1,l1);
+  print_list(head );
   printf("==\n");
-  print_list();
-  delete_element(&d2,&l2);
+  beg_del_element(&head, &d2, &l2);
   printf("the stuff that we got: %s %d\n",d2,l2);
+  print_list(head);
   printf("@@\n");
-  print_list();
+  beg_del_element(&head, &d2, &l2);
+  printf("the stuff that we got: %s %d\n",d2,l2);
+  print_list(head);
+  printf("$$\n");
+  beg_del_element(&head, &d2, &l2);
+  print_list(head);
+
 }
